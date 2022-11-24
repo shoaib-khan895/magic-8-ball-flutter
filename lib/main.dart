572b6239c0fb65e34f7af2eab64a998ab17e574f
shoaib-khan895 <71784547+shoaib-khan895@users.noreply.github.com>
2022-11-24 12:39:15 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('8 Ball'),
+          backgroundColor: Colors.red,
+        ),
+        body: Ball(),
+      ),
+    ),
+  );
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int x = 3;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child:
+          Expanded(
+            child: TextButton(
+                child: Image.asset('images/ball$x.png'),
+                onPressed: () {
+                  setState(() {
+                    x = Random().nextInt(5) + 1;
+                  });
+                }),
+          )
+        ,
       ),
     );
+  }
+}
